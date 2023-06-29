b355f0063005b3bbd7b3a5697ddc5ee344d8a6ef
Victor Micheni <victormichenim@gmail.com>
2023-06-29 13:08:52 +0300

    Added a book class which inherits from publication

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -4,7 +4,21 @@ public class Example
 {
     public static void Main()
     {
-        var Bentley = new Automobile("Slime", "Intercontinental", 2022);
-        Console.WriteLine(Bentley);
+        // var Bentley = new Automobile("Slime", "Intercontinental", 2022);
+        // Console.WriteLine(Bentley);
+
+        var book = new Book("The Tempest", "0912557348", "Shakespeare, William", "Public Domain Press");
+        ShowPublicationInfo(book);
+        book.Publish(new DateTime(2016, 8, 18));
+        ShowPublicationInfo(book);
+
+  
+
+    }
+
+    public static void ShowPublicationInfo(Publication pub)
+    {
+        string pubDate = pub.GetPublicationDate();
+        Console.WriteLine($"{pub.Title}, " + $"{(pubDate == "NYP" ? "Not Yet Published" : "Published on " + pubDate):d} by {pub.Publisher}");
     }
 }
--- Inheritance/Publication.cs
+++ Inheritance/Publication.cs
@@ -8,8 +8,65 @@ public abstract class Publication
 
     public Publication(string title, string publisher, PublicationType type)
     {
+        if (string.IsNullOrEmpty(title))
+            throw new ArgumentException("The title is required");
+        Title = title;
 
+        if (string.IsNullOrEmpty(publisher))
+            throw new ArgumentException("ThePublisher is required");
+        Publisher = publisher;
+
+        Type = type;
     }
+
+    public string Title { get; }
+
+    public string Publisher { get; }
+
+    public PublicationType Type { get; }
+
+    public string? CopyrightName { get; private set; }
+
+    public int CopyrightDate { get; private set; }
+
+    public int Pages
+    {
+        get { return _totalPages; }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentOutOfRangeException(nameof(value), "The number of pages cannot be zero or negative ");
+            _totalPages = value;
+        }
+    }
+
+    public string GetPublicationDate()
+    {
+        if (!_published)
+            return "NYP";
+        else
+            return _datePublished.ToString("d");
+    }
+
+    public void Publish(DateTime datePublished)
+    {
+        _published = true;
+        _datePublished = datePublished;
+    }
+
+    public void Copyright(string copyrightName, int copyrightDate)
+    {
+        if (string.IsNullOrEmpty(copyrightName))
+            throw new ArgumentException("The name of the copyright holder is required");
+        CopyrightName = copyrightName;
+
+        int currentYear = DateTime.Now.Year;
+        if (copyrightDate < currentYear - 10 || copyrightDate > currentYear + 2)
+            throw new ArgumentOutOfRangeException($"The copyright Date must be between {currentYear - 10} and {currentYear + 1}");
+        CopyrightDate = copyrightDate;
+    }
+
+    public override string ToString() => Title;
     
 
 }
