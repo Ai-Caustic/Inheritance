9a5efe698d822a065444767540936fcadf23642a
Victor Micheni <victormichenim@gmail.com>
2023-06-29 11:30:44 +0300

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        var Bentley = new Automobile("Slime", "Intercontinental", 2022);
+        Console.WriteLine(Bentley);
+    }
+}
---
+++ Inheritance/Publication.cs
@@ -0,0 +1,15 @@
+﻿public enum PublicationType { Misc, Book, Magazine, Article };
+
+public abstract class Publication
+{
+    private bool _published = false;
+    private DateTime _datePublished;
+    private int _totalPages;
+
+    public Publication(string title, string publisher, PublicationType type)
+    {
+
+    }
+    
+
+}
