7132891bcf408c6e4dd713e7d9d57e940544a1a9
Victor Micheni <victormichenim@gmail.com>
2023-06-29 13:37:44 +0300

    Added Context in models and installed packages

---
+++ Inheritance/Magazine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public sealed class Magazine : Publication
+{
+    public Magazine(string title, string author, string publisher) : this(title, string.Empty, author, publisher)
+    { }
+
+    public Magazine(string title, string genre, string author, string publisher) : base(title, publisher, PublicationType.Magazine)
+    {
+        Author = author;
+        Genre = genre;
+    }
+
+    public string Genre { get; }
+
+    public string Author { get; }
+
+
+
+}   
